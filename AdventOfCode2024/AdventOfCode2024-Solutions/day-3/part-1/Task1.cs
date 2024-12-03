c492dde7822b59a87a8cfe664c9147e22e2868b9
IliyanTodorov <iliyan.tdrv@gmail.com>
2024-12-03 21:28:55 +0200

    Solve Day 3: Mull It Over- Part 1 and Part 2

---
+++ AdventOfCode2024/AdventOfCode2024-Solutions/day-3/part-1/Task1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024_Solutions.day_3
+{
+    public class Task1
+    {
+        public static void Main(string[] args)
+        {
+            string input = File.ReadAllText("../../../input.txt");
+
+            string pattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            int sum = 0;
+            foreach (Match match in matches)
+            {
+                int x = int.Parse(match.Groups[1].Value);
+                int y = int.Parse(match.Groups[2].Value);
+
+                sum += x * y;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ AdventOfCode2024/AdventOfCode2024-Solutions/day-3/part-2/Task2.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024_Solutions.day_3.part_2
+{
+    public class Task2
+    {
+        public static void Main(string[] args)
+        {
+            string input = File.ReadAllText("../../../input.txt");
+
+            string pattern = @"mul\((\d{1,3}),(\d{1,3})\)|(don't\(\)|do\(\))";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            int sum = 0;
+            bool isEnaibled = true;
+            foreach (Match match in matches)
+            {
+                switch (match.Value)
+                {
+                    case "do()":
+                        isEnaibled = true;
+                        continue;
+                    case "don't()":
+                        isEnaibled = false;
+                        continue;
+                    default:
+                        if (!isEnaibled) continue;
+
+                        int x = int.Parse(match.Groups[1].Value);
+                        int y = int.Parse(match.Groups[2].Value);
+
+                        sum += x * y;
+
+                        break;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
